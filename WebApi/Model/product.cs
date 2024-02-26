426811302ea110216adf9b9e03b0417707bab3de
Vinicius Gabriel Cabral Paulino <vgcpaulino@gmail.com>
2024-02-26 18:59:06 +0000

    feat: Creation of WebApi

---
+++ WebApi/Model/product.cs
@@ -0,0 +1,12 @@
+
+public class ProductModel {
+    public required string Id { get; set; } = string.Empty;
+    public required string Department { get; set; }
+    public required string Isbn { get; set; }
+    public required string Price { get; set; }
+    public required string Product { get; set; }
+    public required string ProductDescription { get; set; }
+    public required string ProductMaterial { get; set; }
+    public required string  ProductName { get; set; }
+
+}
